3b0c9623966f12c6a4b4b47f334fa7470482046f
Ard1tH <ardit.haklaj@student.uni-pr.edu>
2023-05-15 18:17:11 +0200

    .

--- faq.php
+++ faq.php
@@ -64,12 +64,6 @@
 }
 
   </style>
-
-
-
-  
-
-  
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
   <meta content="Free HTML Templates" name="keywords">
   <meta content="Free HTML Templates" name="description">
