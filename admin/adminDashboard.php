7412b6ccb83ce919762ede5b054ffa5ba105fe8b
Dion-Kastrati <122050394+Dion-Kastrati@users.noreply.github.com>
2023-05-21 22:53:23 +0200

    .

--- admin/adminDashboard.php
+++ admin/adminDashboard.php
@@ -59,19 +59,13 @@
                             </a>
                             <div class="collapse" id="collapsePages" aria-labelledby="headingTwo" data-bs-parent="#sidenavAccordion">
                                 <nav class="sb-sidenav-menu-nested nav accordion" id="sidenavAccordionPages">
-                                    <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#pagesCollapseError" aria-expanded="false" aria-controls="pagesCollapseError">
-                                        User Pages
-                                        <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                                    </a>
-                                    <div class="collapse" id="pagesCollapseError" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordionPages">
-                                        <nav class="sb-sidenav-menu-nested nav">
+                                    <nav class="sb-sidenav-menu-nested nav">
                                         <a href="../index.php" class="nav-item nav-link">Home</a>
                                         <a href="../shop.php" class="nav-item nav-link">Books</a>
                                         <a href="../cart.php" class="nav-item nav-link">Shopping Cart</a>
                                         <a href="../favorites.php" class="nav-item nav-link">Favorites</a>
                                         <a href="../checkout.php" class="nav-item nav-link">Checkout</a>
-                                        </nav>
-                                    </div>
+                                    </nav>
                                 </nav>
                             </div>
                         </div>
@@ -89,7 +83,7 @@
                             <li class="breadcrumb-item active">Dashboard</li>
                         </ol>
                         <div class="row">
-                            <div class="col-xl-4 col-md-12">
+                            <div class="col-xl-6 col-md-12">
                                 <div class="card bg-primary text-white mb-4">
                                     <div class="card-body">Users</div>
                                     <div class="card-footer d-flex align-items-center justify-content-between">
@@ -98,20 +92,11 @@
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-xl-4 col-md-12">
+                            <div class="col-xl-6 col-md-12">
                                 <div class="card bg-warning text-white mb-4">
                                     <div class="card-body">Books</div>
                                     <div class="card-footer d-flex align-items-center justify-content-between">
                                         <a class="small text-white stretched-link" href="Books.php">View Details</a>
-                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-xl-4 col-md-12">
-                                <div class="card bg-success text-white mb-4">
-                                    <div class="card-body">Orders</div>
-                                    <div class="card-footer d-flex align-items-center justify-content-between">
-                                        <a class="small text-white stretched-link" href="#">View Details</a>
                                         <div class="small text-white"><i class="fas fa-angle-right"></i></div>
                                     </div>
                                 </div>
@@ -135,11 +120,6 @@
                                     
                                     <tbody>
                                     <?php
-                                        $dbServerName = "localhost:3307";
-                                        $dbUsername = "root";
-                                        $dbPassword = "";
-                                        $dbName = "web2-database";
-                                        
                                         // Create connection
                                         $conn = new mysqli($dbServerName, $dbUsername, $dbPassword, $dbName);
                                         // Select data from database
